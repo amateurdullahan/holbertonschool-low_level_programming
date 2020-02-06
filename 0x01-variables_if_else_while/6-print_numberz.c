487747f03bf0236d83c4514780da9bcf3a2c33f0
amateurdullahan <zabrak12@gmail.com>
2020-02-06 22:44:38 +0000

    add 6-print_numberz, is to Betty standards

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Return: value is 0 is positive
+ */
+int main(void)
+{
+  char n = 48;
+  while (n < 58)
+    {
+      putchar(n);
+	n++;
+    }
+  putchar(10);
+  return (0);
+}
