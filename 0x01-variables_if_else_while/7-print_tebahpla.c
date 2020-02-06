ff9bb33af5248848b5b7ee61a23765a9f2b7a36d
amateurdullahan <zabrak12@gmail.com>
2020-02-06 22:49:38 +0000

    add 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - description
+ *
+ * Description: This function uses the ascii value and
+ * that char is an integer to print the alphabet
+ * With each increment it moves to the next letter in the alphabet until it
+ * reaches z, upon which it prints z and then a newline
+ *
+ * Return: value is 0 which is positive
+ */
+int main(void)
+{
+  char letter = 'z';
+  while (letter >= 'a')
+    {
+      putchar(letter);
+      letter--;
+    }
+  putchar('\n');
+  return (0); /*returns value of 0*/
+}
