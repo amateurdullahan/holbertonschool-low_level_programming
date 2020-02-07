b4dc974f614f6f04c0ec41544cc78e0909eec57a
amateurdullahan <zabrak12@gmail.com>
2020-02-07 02:15:08 +0000

    fix Betty coding style mistakes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-
+/**
+ * main - print strings
+ *
+ * Description - print the byte value of various datatypes
+ * on individual lines
+ *
+ * Return: value is 0
+ */
 int main(void)
 {
-  printf("Size of a char: %lu byte(s)\n", sizeof(char));
-  printf("Size of an int: %lu byte(s)\n", sizeof(int));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-  printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
-  return (0);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
