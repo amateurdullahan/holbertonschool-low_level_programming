5cd58809f5e51a1adb771a3bf2bee0bce2319713
amateurdullahan <zabrak12@gmail.com>
2020-02-10 20:47:40 +0000

    add 4-isalpha, update header

--- 0x02-function_nested_loops/holberton.h
+++ 0x02-function_nested_loops/holberton.h
@@ -51,3 +51,20 @@ int _islower(int c)
       return (0);
     }
 }
+
+int _isalpha(int c)
+{
+  if (c >= 'a' && c <= 'z')
+    {
+      return (1);
+    }
+  else if (c >= 'A' && c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
+
