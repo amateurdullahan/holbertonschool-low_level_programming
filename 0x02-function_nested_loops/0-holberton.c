3312ffa8f634c01265bf08fa92ac4448fdb6e2c0
amateurdullahan <zabrak12@gmail.com>
2020-02-10 16:55:51 +0000

    add README, holberton header and 0-holberton.c

---
+++ 0x02-function_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - void
+ *
+ * Description: a description for the program
+ *
+ * return - 0
+ */
+
+int main(void)
+{
+
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+ return(0);
+}
