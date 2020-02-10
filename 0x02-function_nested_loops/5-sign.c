a84217788ac218b95e43a6d5d0ecbdae7f6f3b02
amateurdullahan <zabrak12@gmail.com>
2020-02-10 20:59:57 +0000

    add 5-sign, update header

---
+++ 0x02-function_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - void
+ *
+ * Return: 0
+ */
+
+int print_sign(int n);
+
+int main(void)
+{
+  print_sign(176);
+  print_sign(22);
+  print_sign(0);
+  return (0);
+}
