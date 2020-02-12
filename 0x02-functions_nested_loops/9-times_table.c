ab5e54b7fefcc96c1e9a136bff12764e7e3eaa8e
amateurdullahan <zabrak12@gmail.com>
2020-02-12 02:24:30 +0000

    add 9-times_table, update header

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * main - void
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+int ch;
+int j;
+int c;
+
+for (ch = 0; ch <= 9; ch++)
+{
+for (j = 0; j <= 9; j++)
+{
+c = ch * j;
+if (c > 9)
+{
+_putchar((c/10) + '0');
+_putchar((c%10) + '0');
+}else
+{
+if (j == 0)
+{
+_putchar(c + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(c + '0');
+}
+}
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar ('\n');
+}
+}
