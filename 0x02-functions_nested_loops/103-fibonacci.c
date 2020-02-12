aa19586b0a841b0c8fc3dec489bd82e07157ef28
amateurdullahan <zabrak12@gmail.com>
2020-02-12 20:02:07 +0000

    worked on 103 with tha homies, added newline for 102

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,19 +8,24 @@
 
 int main(void)
 {
+  long e = 2;
   long p = 3;
   long pp = 2;
   long ppp = 1;
   int c;
 
-  printf("1, 2, 3, ");
+  printf("2, ");
 
-  for( c = 0; c < 47; c++)
+  for( c = 0; c < 50; c++)
     {
       p = p + pp;
       pp = pp + ppp;
       ppp = pp - ppp;
-      printf("%ld, ", p);
+      if((p % 2) == 0 && p <= 4000000)
+	{
+	  e = e + p;
+	  printf("%ld, ", e);
+	}
     }
   printf("\n");
   return (0);
