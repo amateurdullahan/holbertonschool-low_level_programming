1ca60630001b5c9215a47354f23426577bef645e
amateurdullahan <zabrak12@gmail.com>
2020-02-13 19:59:36 +0000

    add 5-more_numbers, update holberton w prototype function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - 0 - 14 10 times
+ *
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+  char o = '0';
+  char t = '1';
+  int c;
+
+  for (c = 0; c < 15; c++)
+    {
+    if (c > 9)
+      {
+	_putchar(t);
+      }
+  _putchar(o);
+  o++;
+  if (o >'9')
+    {
+      o = '0';
+    }
+    }
+  _putchar('\n');
+}
