9b3550ed427070841f2259c2469794fb07ad59fc
amateurdullahan <zabrak12@gmail.com>
2020-02-13 22:04:03 +0000

    add 9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-100, plus fizzbuzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  int c;
+  for(c = 1; c < 101; c++)
+    {
+      if((c % 3) == 0)
+	{
+	  printf("Fizz ");
+	}
+      else if ((c % 5) == 0)
+	{
+	  printf("Buzz ");
+	}
+      else
+	printf("%d ", c);
+    }
+  printf("\n");
+  return (0);
+}
