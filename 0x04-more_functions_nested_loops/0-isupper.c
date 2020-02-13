5d4057faeec1f65e0b3bb6592a71d7633a2ad828
amateurdullahan <zabrak12@gmail.com>
2020-02-13 19:20:24 +0000

    add 0-isupper, _putchar, holberton header

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - defined
+ *
+ * Return: 1 is uppercase else 0
+ */
+
+int _isupper(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    return (0);
+}
