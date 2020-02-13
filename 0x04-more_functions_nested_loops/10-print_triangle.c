77ca368a80b37989cc0495fb85857fb221250e54
amateurdullahan <zabrak12@gmail.com>
2020-02-13 22:58:46 +0000

    adx 10-print_triangle, update header

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * main - def
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+  int s;
+  int h;
+  int r;
+
+  if(size <= 0)
+    {
+      _putchar('\n');
+    }
+  else if (size > 0)
+    {
+    for (r = 0; r < size; r++)
+      {
+	for (s = (size - r); s > 1; s--)
+	{
+	    _putchar(' ');
+	}
+	for (h = 0; h <= r; h++)
+	  {
+	    _putchar('#');
+	  }
+	_putchar('\n');
+      }
+    }
+}
