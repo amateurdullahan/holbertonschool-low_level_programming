26cced997389043b804396b56ab52c3a4d4e65c3
amateurdullahan <zabrak12@gmail.com>
2020-02-13 21:49:19 +0000

    add CORRECT file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - make a square with hashtags
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+  int row;
+  int column;
+
+  if (size <= 0)
+    {
+      _putchar('\n');
+      return;
+    }
+  else
+    for (column = 0; column < size; column++)
+      {
+	row = 0;
+	while (row < size)
+	  {
+	    _putchar(35);
+	    row++;
+	  }
+	_putchar('\n');
+      }
+}
