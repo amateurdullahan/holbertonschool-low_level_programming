16c577687490308d3543ac16e93af0fcd49b8159
amateurdullahan <zabrak12@gmail.com>
2020-02-13 22:13:27 +0000

    fix output of fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,17 @@ int main(void)
 {
   int c;
   for(c = 1; c < 101; c++)
-    {
-      if((c % 3) == 0)
+    {     
+      if((c % 3) == 0 || (c % 5) == 0)
+	{      
+	  if((c % 3) == 0)
 	{
 	  printf("Fizz");
 	}
-      else if ((c % 5) == 0)
+      if ((c % 5) == 0)
 	{
 	  printf("Buzz");
+	}
 	}
       else
 	printf("%d", c);
