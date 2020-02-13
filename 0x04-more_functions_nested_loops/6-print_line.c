f3a51c6228045eab6021fc2894a1e7b3233d5c36
amateurdullahan <zabrak12@gmail.com>
2020-02-13 20:53:21 +0000

    add 6-print_line, update holberton header

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - print line n times
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+  int c = 0;
+  if (n == 0)
+    {
+      _putchar('\n');
+      return;
+    }
+  while(c < n)
+    {
+      _putchar('_');
+      c++;
+    }
+  _putchar('\n');
+}
