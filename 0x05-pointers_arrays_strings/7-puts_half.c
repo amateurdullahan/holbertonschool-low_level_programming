951e648bd27edab86c4cfa74590f200161a0af93
amateurdullahan <zabrak12@gmail.com>
2020-02-18 19:19:12 +0000

    add 7-puts_half, incorrect answer but pushing anyways

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * main - c
+ *
+ * Return: no thing
+ */
+
+void puts_half(char *str)
+{
+  int c;
+  int l;
+  int h;
+
+  for (l = 0; str[l] != '\0'; l++)
+    {
+
+    }
+  if (l % 2 != 0)
+    {
+      h = (l - 1) / 2;
+    }
+  else
+    {
+      h = l / 2;
+    }
+  for(c = l; c > h; c--)
+    {
+      _putchar(str[c]);
+    }
+  _putchar('\n');
+}
