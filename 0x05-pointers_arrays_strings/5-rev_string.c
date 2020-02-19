2010cfc4969c291a1e1d5a5b90da667a8d70e1e6
amateurdullahan <zabrak12@gmail.com>
2020-02-19 01:22:18 +0000

    fix typos and betty style

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,20 @@
 
 void rev_string(char *s)
 {
-  int t;
-  int c;
-  int l;
-  char q;
+int t;
+int c;
+int l;
+char q;
 
-  for (t = 0; s[t] != '\0'; t++)
-    {
-    }
-  l = t;
+for (t = 0; s[t] != '\0'; t++)
+{
+}
+l = t;
 
-  for (i--, c = 0; c < l / 2; i--, c++)
-    {
-      q = s[c];
-      s[c] = s[i];
-      s[i] = h;
-    }
+for (t--, c = 0; c < l / 2; t--, c++)
+{
+q = s[c];
+s[c] = s[t];
+s[t] = q;
+}
 }
