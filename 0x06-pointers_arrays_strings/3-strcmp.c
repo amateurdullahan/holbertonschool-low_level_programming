872e7b8245fcf2938775c6f15181106291696710
amateurdullahan <zabrak12@gmail.com>
2020-02-20 20:43:19 +0000

    add 3-strcmp.c, update header

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares strings
+ *
+ * Return - nothing
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+  int f;
+  int v1;
+  int v2;
+  int sum = 0;
+
+  for (f = 0; s1[f] != '\0'; f++)
+    {
+      v1 = s1[f];
+      v2 = s2[f];
+      if (v1 > v2 || v1 < v2)
+	{
+	  sum = (v1 - v2);
+	  break;
+	}
+    }
+  return (sum);
+}
