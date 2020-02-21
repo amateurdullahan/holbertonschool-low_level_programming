70f2bd7d43e9b46b2bd3f66a0428200e01aae3f1
amateurdullahan <zabrak12@gmail.com>
2020-02-21 00:21:19 +0000

    betty stuff

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,13 @@
 
 /**
  * strncat - does stuff
- *
  * @dest - dest
  * @src - src
  * @n - int
+ *
+ * Description: its a function
+ *
+ * Return: oh wow
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -14,7 +17,6 @@ int c;
 int l;
 
 for (c = 0; dest[c] != '\0'; c++);
-
 for (l = 0; l < n && src[l] != '\0'; l++, c++)
 {
 dest[c] = src[l];
