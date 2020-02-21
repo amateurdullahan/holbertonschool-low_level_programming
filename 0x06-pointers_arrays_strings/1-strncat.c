a7bbc5634e1a62d143506a1cda02185a76ee66d3
amateurdullahan <zabrak12@gmail.com>
2020-02-21 00:19:07 +0000

    betty stuff

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,27 +6,23 @@
  * @dest - dest
  * @src - src
  * @n - int
- * Return: pointer to dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int c;
-  int l;
+int c;
+int l;
 
-  for (c = 0; dest[c] != '\0'; c++)
-    {
-    }
+for (c = 0; dest[c] != '\0'; c++);
 
-  for (l = 0; l < n && src[l] != '\0'; l++, c++)
-    {
-      dest[c] = src[l];
-    }
-    if (dest[c] != '\0')
-    {
-      c++;
-      dest[c] = '\0';
-    }
-
-  return (dest);
+for (l = 0; l < n && src[l] != '\0'; l++, c++)
+{
+dest[c] = src[l];
+}
+if (dest[c] != '\0')
+{
+c++;
+dest[c] = '\0';
+}
+return (dest);
 }
