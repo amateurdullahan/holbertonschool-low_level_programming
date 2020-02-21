066cab0f6c2544836bdcea95ce85619a2188732c
amateurdullahan <zabrak12@gmail.com>
2020-02-21 00:22:15 +0000

    betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,8 @@ char *_strncat(char *dest, char *src, int n)
 int c;
 int l;
 
-for (c = 0; dest[c] != '\0'; c++);
+for (c = 0; dest[c] != '\0'; c++)
+;
 for (l = 0; l < n && src[l] != '\0'; l++, c++)
 {
 dest[c] = src[l];
