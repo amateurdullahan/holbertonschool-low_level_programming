00d207203cfe40075fd3bf7f21e86d8e4d47eef6
amateurdullahan <zabrak12@gmail.com>
2020-02-21 22:14:16 +0000

    add empty files of 102-infinite_add and 103-print_buffer

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,16 @@
+/**
+ * *infinite_add - add the infinite
+ * @n1 - first number
+ * @n2 - second number
+ * @r - buffer
+ * @size_r - size of buffer
+ *
+ * Return: value of addition
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+
+  return (0);
+}
---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,13 @@
+/**
+ * print_buffer - print the buffer yo
+ * @b - its the buff
+ * @size - size of the buff
+ *
+ * Return - size or 0
+ */
+
+void print_buffer(char *b, int size)
+{
+
+  return(0);
+}
