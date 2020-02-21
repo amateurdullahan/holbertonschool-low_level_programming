7d3a15546968e1e6b8301c5e6e6402e8263b60c7
amateurdullahan <zabrak12@gmail.com>
2020-02-21 00:25:46 +0000

    fix betty stuff

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 
 /**
- * *_strncpy - dont copy that floppy
+ * *_strncpy - dont copy that floppy 
+ * @dest - its the destination
+ * @src - its the source
+ * @n - its the number
  *
  * Return: your copied floppy
  */
 
+
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i;
+int i;
 
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    dest[i] = src[i];
-  for( ; i < n; i++)
-    dest[i] = '\0';
-
-  return dest;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (i < n; i++)
+dest[i] = '\0';
+return (dest);
 }
