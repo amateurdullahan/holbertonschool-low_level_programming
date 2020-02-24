a459a14546453b6809cdb1fda469628e6483891f
amateurdullahan <zabrak12@gmail.com>
2020-02-24 17:24:44 +0000

    base structure of 4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+/**
+ * *_strpbrk - searches string for set of bytes
+ * @s: string
+ * @accept: bytes
+ *
+ * Return: pointer to byte that matches tring
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+}
