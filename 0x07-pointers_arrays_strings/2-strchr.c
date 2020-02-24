318345ede7e0fb7b0eb5bdd022bd0d1c84555872
amateurdullahan <zabrak12@gmail.com>
2020-02-24 16:42:09 +0000

    add 2-strchr and updated holberton.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * *_strchr - find first occurence of c in s
+ * @s: the string
+ * @c: character we are looking for
+ *
+ * Return: s if c found or null if not
+ */
+
+char *_strchr(char *s, char c)
+{
+  int i;
+
+
+  for (i = 0; s[i]; i++)
+    {
+      if (s[i] == c)
+	{
+	  return (&s[i]);
+	}
+    }
+    return ('\0');
+}
