c2c2bc4874aef973b613e11922015da083049369
amateurdullahan <zabrak12@gmail.com>
2020-02-24 19:23:41 +0000

    betty fixes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,14 @@
 
 char *_strchr(char *s, char c)
 {
-  int i;
+int i;
 
-
-  for (i = 0; s[i]; i++)
-    {
-      if (s[i] == c)
-	{
-	  return (&s[i]);
-	}
-    }
-    return ('\0');
+for (i = 0; s[i]; i++)
+{
+if (s[i] == c)
+{
+return (&s[i]);
+}
+}
+return ('\0');
 }
