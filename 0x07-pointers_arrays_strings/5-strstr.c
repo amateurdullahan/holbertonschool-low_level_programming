fec704f3d29c19ec5614bebc46ded1bf38116ed3
amateurdullahan <zabrak12@gmail.com>
2020-02-24 20:22:17 +0000

    fix output and betty style issues

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,34 +8,31 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-  int c, d, st, address;
-  /* char store;*/
-  d = 0;
+int c, d, st, address;
 
-    for (c = 0; haystack[c]; c++)
-      {
-	while (needle[d] != '\0')
-	  {
-	    if (haystack[c] == needle[d])
-	      {
-		address = c;
-		st = c;
-		while (haystack[c] == needle[d])
-		  {
-		    c++;
-		    d++;
-		  }
-		if (haystack[c] == needle[d])
-		  {
-		    return (&haystack[address]);
-		  }
-		else
-		  {
-		    c = st;
-		    break;
-		  }
-	      }
-	  }
-      }
-	return (0);
+d = 0;
+
+for (c = 0; haystack[c]; c++)
+{
+if (haystack[c] == needle[d])
+{
+address = c;
+st = c;
+while (haystack[c] == needle[d] && haystack[c] != '\0' && needle[d] != '\0')
+{
+c++;
+d++;
+}
+if (haystack[c] == needle[d])
+{
+return (&haystack[address]);
+}
+else
+{
+c = st;
+break;
+}
+}
+}
+return (0);
 }
