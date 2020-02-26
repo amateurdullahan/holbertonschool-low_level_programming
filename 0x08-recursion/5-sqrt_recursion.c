fdd8a2f8d79e16c551b167404e6c38cd9cf28206
amateurdullahan <zabrak12@gmail.com>
2020-02-26 23:00:19 +0000

    fix betty issues in 3-factorial

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _sqrt_recursion - return natural square root
+ * @n: the aforementioned number
+ *
+ * Return: natural square root OR -1
+ */
+
+int _sqrt_recursion(int n)
+{
+
+else
+return (-1);
+}
