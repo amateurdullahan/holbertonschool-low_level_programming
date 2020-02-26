40b0c54adf9ae9b34da7f8feca36f961f6c238e5
amateurdullahan <zabrak12@gmail.com>
2020-02-26 21:50:25 +0000

    add base 4-pow_recursion and update header

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _pow_recursion - x to the y power
+ * @x: 1st integer
+ * @y: to the power of
+ *
+ * Return: x to power of y OR -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1)
+}
+else
+}
