2c1459404e867a21d47b180ed7ff6f4346fccf72
amateurdullahan <zabrak12@gmail.com>
2020-02-26 21:38:13 +0000

    add base 3-factorial, update header

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/**
+ * factorial - lets do some maths
+ * @n : the number that needs to be math'd
+ *
+ * Return: factorial OR -1
+ */
+
+int factorial(int n)
+{
+  if( n < 0)
+    {
+      return (-1);
+    }
+  else if (n == 1 && n == 0)
+    {
+      return (1);
+    }
+  else
+    {
+  return (n);
+    }
+}
