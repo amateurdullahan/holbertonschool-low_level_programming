457d96f6f19c9171cbb55e14daeaf19d4ba42549
amateurdullahan <zabrak12@gmail.com>
2020-02-26 22:55:35 +0000

    add base 6-is_prime_number, 7-is_palindrome, update header

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+/**
+ * is_prime_number - determines is a number is prime
+ * @n: integer being checked
+ *
+ * Return: 1 is prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+}
---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,14 @@
+/**
+ * is_palindrome - determines if string is palindrome
+ * @s: string to be checked
+ *
+ * Return: 1 if palindrome OR 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+if (s == '\0')
+{
+return 1;
+}
+}
