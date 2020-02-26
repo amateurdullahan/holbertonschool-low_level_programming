58e1efb5ff99df95f9b6d1d5af5534bbccda2b49
amateurdullahan <zabrak12@gmail.com>
2020-02-26 17:18:33 +0000

    fix betty style in 0-puts_recursion, add base 1-print_rev_recursion, update header

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts_recursion - recursively prints string
- * @s - string to be printed
+ * @s: string to be printed
  */
 
 void _puts_recursion(char *s)
 {
- if (*s != '\0')
-    {
-      _putchar(*s);
-      s++;
-      _puts_recursion(s);
-    }
-  else
-    _putchar('\n');
+if (*s != '\0')
+{
+_putchar(*s);
+s++;
+_puts_recursion(s);
+}
+else
+_putchar('\n');
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string reverse
+ * @s: string in question
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+}
