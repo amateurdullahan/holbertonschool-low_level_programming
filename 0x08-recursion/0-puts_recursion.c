bb51f9c097d6286c0e41a39dde5a2353191de22a
amateurdullahan <zabrak12@gmail.com>
2020-02-26 17:13:54 +0000

    correct output for 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,14 @@
  * @s - string to be printed
  */
 
-void _puts_recusion(char *s)
+void _puts_recursion(char *s)
 {
-
+ if (*s != '\0')
+    {
+      _putchar(*s);
+      s++;
+      _puts_recursion(s);
+    }
+  else
+    _putchar('\n');
 }
