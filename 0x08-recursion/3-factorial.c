07bc91527c563d408c9de5d72009e87d933783e1
amateurdullahan <zabrak12@gmail.com>
2020-02-26 21:43:29 +0000

    correct output for 3-factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,12 +11,12 @@ int factorial(int n)
     {
       return (-1);
     }
-  else if (n == 1 && n == 0)
+   else if (n == 1 || n == 0)
     {
       return (1);
-    }
+      }
   else
     {
-  return (n);
+      return (n * factorial(n - 1));
     }
 }
