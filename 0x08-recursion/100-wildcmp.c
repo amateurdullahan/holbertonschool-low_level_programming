5dd63ef937e5775d89e4426edac6089023353b9b
amateurdullahan <zabrak12@gmail.com>
2020-02-27 19:49:38 +0000

    add update 100-wildcmp

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * wildskip - skip all wildcards
  * @s1: first string
@@ -7,26 +5,31 @@
  *
  */
 
-void wildskip(char *s1, char *s2)
+int wildskip(char *s2)
 {
-  if (*s1 == '*')
-    {
-      s1++;
-    }
   if (*s2 == '*')
     {
-      s2++;
+      return(wildskip(s2 + 1));
     }
 }
 
 
 /**
  * cmpstr - the actual comparison
+ * @s1: string one
+ * @s2: string two
  */
-
-/**
- *
- */
+int cmpstr(char *s1, char *s2)
+{
+  if (s1 != s2)
+    {
+      return (0);
+    }
+  else if (*s1 && *s2 && s1 == s2)
+    return cmpstr(++s1, wildskip(++s2));
+  else
+    return (1);
+}
 
 /**
  * wildcmp - compare two strings
@@ -36,5 +39,5 @@ void wildskip(char *s1, char *s2)
 
 int wldcmp(char *s1, char *s2)
 {
-  return ();
+  return (cmpstr(s1, s2));
 }
