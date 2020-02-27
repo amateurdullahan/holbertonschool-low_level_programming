f5134273dcc4cf43fff9edcbff807debcdf8441a
amateurdullahan <zabrak12@gmail.com>
2020-02-27 01:19:21 +0000

    correct output for 6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,29 @@
 /**
- * is_prime_number - determines is a number is prime
+ * checker - checks primes
+ * @c - number to divide in
+ * @prime - number divided into
+ *
+ * Return: 1 if prime, 0 if not
+ */
+
+int checker(c, prime)
+{
+if (prime % c == 0 || prime < 2)
+{
+return (0);
+}
+else if (c == prime - 1)
+{
+return (1);
+}
+else if (prime > c)
+{
+return (checker(c + 1, prime));
+}
+}
+
+/**
+ * is_prime_number - is a number is prime
  * @n: integer being checked
  *
  * Return: 1 is prime, 0 if not
@@ -7,4 +31,5 @@
 
 int is_prime_number(int n)
 {
+  return (checker(2, n));
 }
