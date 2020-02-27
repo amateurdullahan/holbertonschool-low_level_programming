d6508c626c53041a75a9bf3b61daf34557bd30b0
amateurdullahan <zabrak12@gmail.com>
2020-02-27 02:46:18 +0000

    correct outputfor 7-is_palindrome, fix betty style

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,3 +1,48 @@
+#include "holberton.h"
+
+/**
+ * palindrome_check - checks for palindrome
+ * @s: string to check
+ * @l: length of string
+ * @c: counter
+ *
+ * Return: 1 for palindrome, 0 for not
+ */
+
+int palindrome_check(char *s, int l, int c)
+{
+if (s[c] == s[l] && l != c)
+{
+return (palindrome_check(s, ++l, ++c));
+}
+if (s[c] == s[l] || c >= 1)
+{
+return (1);
+}
+if (s[c] != s[l])
+{
+return (0);
+}
+return (1);
+}
+
+/**
+ * r_strlen - recursive check of string length
+ * @s: string to check length
+ * @i: counter
+ *
+ * Return: return length
+ */
+
+int r_strlen(char *s, int i)
+{
+if (s[i] != '\0')
+{
+return (r_strlen(s, ++i));
+}
+return (--i);
+}
+
 /**
  * is_palindrome - determines if string is palindrome
  * @s: string to be checked
@@ -7,8 +52,9 @@
 
 int is_palindrome(char *s)
 {
-if (s == '\0')
-{
-return 1;
-}
+int len;
+
+len = r_strlen(s, 0);
+
+return (palindrome_check(s, len, 0));
 }
