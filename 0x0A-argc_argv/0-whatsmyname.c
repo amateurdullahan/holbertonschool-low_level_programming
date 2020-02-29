c63b75d183b6664b5ab5c83886f2b62e730fa448
amateurdullahan <zabrak12@gmail.com>
2020-02-29 19:10:07 +0000

    fix unsued variable error, betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+ * main - print name of program
+ * @argc: unused attribute
+ * @argv: pointer array of arguments
+ *
+ * Return: 0 which is positive
+ */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-  printf("%s", argv[0]);
-  putchar('\n');
-  return (0);
+printf("%s", argv[0]);
+putchar('\n');
+return (0);
 }
