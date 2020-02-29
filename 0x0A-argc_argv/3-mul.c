282e597b02082c203cf38c220bd64b2556966217
amateurdullahan <zabrak12@gmail.com>
2020-02-29 19:53:15 +0000

    add 3-mul, not working quite right

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - muliply two arguments 
+ * @argc: arguement counter
+ * @argv: pointer array of arguments
+ *
+ * Return: 0 which is positive OR 1 which is failure
+ */
+
+int main(int argc, char *argv[])
+{
+  int a, b;
+
+  a = argv[1];
+  b = argv[2];
+  if (argc < 2)
+    {
+      printf("Error\n");
+      return 1;
+    }
+  else
+    printf("%d\n", (a * b));
+  return (0);
+}
