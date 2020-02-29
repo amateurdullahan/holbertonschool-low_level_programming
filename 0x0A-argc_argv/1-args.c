7c350b631654542d1f27f81f23d1479ebc0b574c
amateurdullahan <zabrak12@gmail.com>
2020-02-29 19:13:36 +0000

    add base 1-args program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer array of arguments
+ *
+ * Return: 0 which is positive
+ */
+
+int main (int argc, char *argv)
+{
+
+  return (0);
+}
