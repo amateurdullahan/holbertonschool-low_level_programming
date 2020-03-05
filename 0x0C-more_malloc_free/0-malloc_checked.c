006deba733479af0a70f4a0e34ef9d4a7a4f0999
amateurdullahan <zabrak12@gmail.com>
2020-03-05 18:22:05 +0000

    add 0-malloc_checked, incomplete, and updated header

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates and then checks memory
+ * @b: size of memory allocated
+ *
+ * Return: pointer to memory OR 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+ void *r;
+
+  r = malloc(b);
+
+  if (r == NULL)
+    {
+      exit(98);
+    }
+    return (r);
+}
