36d577ad8244b2eb2668dcb38df5c526f2e68d52
amateurdullahan <zabrak12@gmail.com>
2020-03-05 20:59:08 +0000

    try using code from tuesday

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,46 +11,29 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *cat;
-unsigned int fl, sl;
+  char *concat;
+unsigned int c, d;
 
-if (s1 == NULL)
-{
-*s1 = '\0';
-}
-if (s2 == NULL)
-{
-*s2 = '\0';
-}
-
-for (fl = 0; s1[fl] != '\0'; fl++)
-;
-
-for (sl = 0; s2[sl] != '\0'; sl++)
-;
-
-if (n >= sl)
-{
-n = sl;
-}
-cat = malloc(sizeof(char) * (fl + n));
- 
-if (cat == NULL)
-{
-free(cat);
-return (NULL);
-}
-
-else
-for (fl = 0; s1[fl] != '\0'; fl++)
-{
-cat[fl] = s1[fl];
-}
-for (sl = 0; sl < n; sl++)
-{
-cat[fl+sl] = s2[sl];
-}
-
-cat[fl+sl+1] = '\0';
-return (cat);
+  for(c = 0; s1[c]; c++)
+    ;
+  for(d = 0; s2[d]; d++)
+    ;
+  if( n >= d)
+    {
+      n = d;
+    }
+  concat = malloc((c + n) * sizeof(char));
+  if (concat == NULL)
+    {
+      return(NULL);
+    }
+  for(c = 0; s1[c]; c++)
+    {
+      concat[c] = s1[c];
+    }
+  for(d = 0; d < n; d++)
+    {
+      concat[c+d] = s2[d];
+    }
+  return (concat);
 }
