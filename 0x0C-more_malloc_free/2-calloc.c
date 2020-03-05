d9aaf5612203fc4fe810b4d3a4a7ae9257f33f91
amateurdullahan <zabrak12@gmail.com>
2020-03-05 19:29:16 +0000

    add base 2-calloc, update header

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocated memory for an array
+ * @nmemb: array elements
+ * @size: size of array
+ *
+ * Return: pointer to array OR NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+
+}
