6fcbc646ca759cb5fe23ef4d54c76dbf97b87481
amateurdullahan <zabrak12@gmail.com>
2020-03-05 20:33:08 +0000

    correct output on local shell

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,24 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-
-
+unsigned int *array;
+unsigned int c = 0;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+else
+array = malloc(sizeof(size) * nmemb);
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
+else
+while (c < nmemb)
+{
+array[c] = 0;
+c++;
+}
+return (array);
 }
