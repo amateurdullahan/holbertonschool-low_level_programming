8f29ec8d3387d65ec200793752a200bc98174d56
amateurdullahan <zabrak12@gmail.com>
2020-03-05 21:17:46 +0000

    add partial 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,11 +11,16 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-/*unsigned int c = 0;*/
+unsigned int c = 0;
 
 if (new_size == old_size)
 {
 return (ptr);
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+return (ptr);
+}
 }
 if (new_size == 0 && ptr != NULL)
 {
