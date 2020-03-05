b7f90d9e374f362321a299226f474825fdffbf03
amateurdullahan <zabrak12@gmail.com>
2020-03-05 20:05:08 +0000

    update 1-string_nconcat, no more seg faults, add header for good measure

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,36 +11,44 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *cat;
+char *cat;
 unsigned int fl, sl;
 
-  for (fl = 0; *s1; fl++)
-    ;
-  cat = malloc(sizeof(char) * (fl + n));
-  if (cat == NULL)
-    {
-      return (NULL);
-    }
+if (s1 == NULL)
+{
+*s1 = '\0';
+}
+if (s2 == NULL)
+{
+*s2 = '\0';
+}
 
-  for (fl = 0; *s1; fl++)
-    {
-      cat[fl] = s1[fl];
-    }
+for (fl = 0; s1[fl] != '\0'; fl++)
+;
 
-  if (s2 == NULL)
-    {
-    s2[0] = '\0';
-    cat[fl + 1] = *s2;
-    }
-  else
-    for (sl = 0; sl < n; sl++)
-      {
-	cat[fl + sl] = s2[sl];
-      }
-  if(sl > n)
-    {
-      cat[fl + sl + 1] = '\0';
-    }
-  return (cat);
+for (sl = 0; s2[sl] != '\0'; sl++)
+;
 
+if (n > sl)
+{
+n = sl;
+}
+cat = malloc(sizeof(char) * (fl + n));
+ 
+if (cat == NULL)
+{
+free(cat);
+return (NULL);
+}
+
+else
+for (fl = 0; s1[fl] != '\0'; fl++)
+{
+cat[fl] = s1[fl];
+}
+for (sl = 0; sl < n; sl++)
+{
+cat[fl+sl] = s2[sl];
+}
+return (cat);
 }
