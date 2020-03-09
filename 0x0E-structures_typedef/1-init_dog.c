114ec50d869f5d8f6c7078c32d9d8208c4d1430d
amateurdullahan <zabrak12@gmail.com>
2020-03-09 18:05:40 +0000

    add 1-init_dog and update dog header

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initalize dog struct
+ * @d: pointer to struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
