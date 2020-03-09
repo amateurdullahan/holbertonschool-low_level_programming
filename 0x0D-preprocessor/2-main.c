9a160918277b0364133720ef12228d08b08b9f54
amateurdullahan <zabrak12@gmail.com>
2020-03-09 17:49:40 +0000

    add 2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of file its complied with
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
