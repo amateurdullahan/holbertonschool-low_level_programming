d7ffd6d232bf490531d4891813e28e0b5b2bd352
amateurdullahan <zabrak12@gmail.com>
2020-03-10 01:46:47 +0000

    add 5-free_dog and dog header

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees allocated memory of dog
+ * @d: dog that needs to be free
+ *
+ * Return: if null
+ */
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+free(d->name);
+free(d->age);
+free(d->owner);
+free(d);
+}
