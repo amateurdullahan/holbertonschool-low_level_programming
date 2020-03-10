9d7dcba6260cd7c81ed9e65ecdd0873713346d6e
amateurdullahan <zabrak12@gmail.com>
2020-03-10 01:56:05 +0000

    base 4-new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * new_dog - uses struct dog to create new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: NULL on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+return;
+}
