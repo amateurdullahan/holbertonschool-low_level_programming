69fc290141065392b37afe62748c115aa126c635
amateurdullahan <zabrak12@gmail.com>
2020-03-11 17:25:33 +0000

    add unfinished 1-array_iterator, finished 0-print_name, updated function_pointers header

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name
+ * print_name - calls a function that prints a name
  * @name: name to be printed
  * @f: function
  *
@@ -11,5 +11,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-
+f(name);
 }
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function as parameter to array
+ * @array: array to be created
+ * @size: size of array
+ * @action: pointer to the function
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+
+}
