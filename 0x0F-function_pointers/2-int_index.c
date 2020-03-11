e5cac3c39dd4a63de92e015330c44e026fcc73a4
amateurdullahan <zabrak12@gmail.com>
2020-03-11 18:13:21 +0000

    2-int_index has correct output, updated header

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer within an array
+ * @array: the array of integers
+ * @size: size of array
+ * @cmp: pointer to function to compare value
+ *
+ * Return: -1 OR more than 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int c;
+if (size <= 0)
+{
+return (-1);
+}
+for(c = 0; c < size; c++)
+{
+cmp(array[c]);
+if (cmp(array[c]) != 0)
+{
+return (c);
+}
+}
+return (-1);
+}
