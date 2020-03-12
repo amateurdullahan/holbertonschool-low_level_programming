aa1d50bafdfadc4d5d46d87a9147c91dd0a10a18
amateurdullahan <zabrak12@gmail.com>
2020-03-12 03:04:20 +0000

    add base code for 100-main_opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcodes of own main function
+ * @argc: number of arguments
+ * @argv: array of arguements
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+return 0;
+}
