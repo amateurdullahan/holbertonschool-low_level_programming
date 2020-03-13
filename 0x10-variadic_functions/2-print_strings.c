628b7b19afca36ab3e87138ec6637adc7a31e8dc
amateurdullahan <zabrak12@gmail.com>
2020-03-13 21:16:49 +0000

    add 2-print_strings, doesnt compile, update header

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - print strings followed by newline
+ * @seperator: the string printed between strings
+ * @n: number of strings passed to function
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+unsigned int c;
+int p;
+va_list string;
+
+va_start(string, n);
+
+for (c = 0; c < n; c++)
+{
+p = va_arg(string, char);
+if (p == NULL)
+{
+printf("(nil)");
+}
+else
+printf("%s", p);
+if (seperator != NULL && c + 1 < n)
+{
+printf("%s", seperator);
+}
+}
+va_end(string);
+printf("\n");
+}
