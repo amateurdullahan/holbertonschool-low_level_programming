4918dc365610c33dd6abf338bfba40f0dbc6c93b
amateurdullahan <zabrak12@gmail.com>
2020-03-13 05:43:53 +0000

    add base files for 0x10 and 0-sum_them_all with one conditional

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - return sum of all parameters
+ * @n: number of parameters
+ *
+ * Return: sum of all parameters OR 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+
+if (n == 0)
+return (0);
+}
