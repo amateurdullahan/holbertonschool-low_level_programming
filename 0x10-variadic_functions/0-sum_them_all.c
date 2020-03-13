5b850591c718c040d1306f49464a9ac6eb4e2582
amateurdullahan <zabrak12@gmail.com>
2020-03-13 20:58:56 +0000

    fix improperly declared va_args, add va_start, correct output

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,19 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list list;
 int sum = 0;
-int c;
+unsigned int c = 0;
 
 if (n == 0)
-return (0);
-
-for (c = 0; c < n; c++)
 {
-  sum += va_arg(list, c);
+return (0);
+}
+
+va_start(list, n);
+
+while (c < n)
+{
+sum += va_arg(list, int);
+c++;
 }
 va_end(list);
 return (sum);
