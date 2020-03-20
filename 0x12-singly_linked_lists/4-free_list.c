f329691ce191a06fa34125a763602fa0c5e30e2e
amateurdullahan <zabrak12@gmail.com>
2020-03-20 00:01:51 +0000

    add 4-free_list, update lists header

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: head of linked list_t list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+if (head == NULL)
+{
+return;
+}
+
+if(head->next != NULL)
+{
+free_list(head->next);
+}
+free(head->str);
+free(head);
+}
