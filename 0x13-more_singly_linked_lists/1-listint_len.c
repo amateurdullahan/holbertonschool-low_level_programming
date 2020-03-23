810da596aef225f8162c0d851f5d9e297469bf63
amateurdullahan <zabrak12@gmail.com>
2020-03-23 22:00:39 +0000

    add 1-listint_len, fix betty issue with 0-print

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,20 +3,19 @@
 #include <stdio.h>
 
 /**
- * print_listint - print number of nodes
+ * listint_len - return length of list
  * @h: const struct pointer conatining list
  *
- * Return: number of nodes
+ * Return: length of list
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t count = 0;
 
 while(h != NULL)
 {
 count++;
-printf("%d\n", h->n);
 h = h->next;
 }
 return (count);
