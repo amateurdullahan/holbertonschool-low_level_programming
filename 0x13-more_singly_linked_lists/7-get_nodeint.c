9e68eb9be90ab9059b54daa1398f20956dd470fc
amateurdullahan <zabrak12@gmail.com>
2020-03-24 20:44:06 +0000

    add 7-get_nodeint, update lists header with prototype

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - returns nth node of listint_t linked list
+ * @head: listint_t list
+ * @index: index of list
+ *
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int count;
+
+for (count = 0; count < index; count ++)
+{
+head = head->next;
+if (head == NULL)
+{
+return (NULL);
+}
+}
+return (head);
+}
