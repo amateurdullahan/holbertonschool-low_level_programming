d533c995edd93f53bfc38a8f77d130bc65aa2e47
amateurdullahan <zabrak12@gmail.com>
2020-03-24 21:19:30 +0000

    add 9-insert_nodeint, almost works, update lists header with prototype

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - insert new node at index
+ * @head: listint_t list
+ * @idx: position in index to insert
+ * @n: data to add to new node
+ *
+ * Return: address of new node OR NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new, *current;
+
+current = *head;
+new = malloc(sizeof(listint_t)); 
+
+if (new == NULL)
+{
+return (NULL);
+}
+
+new->n = n;
+
+if (idx == 0)
+{
+new->next = current;
+*head = new;
+return (*head);
+}
+
+while (idx > 0)
+{
+current = current->next;
+if (current == NULL)
+{
+free(new);
+return (NULL);
+}
+ idx--;
+}
+new->next = current->next;
+current->next = new;
+return (new);
+}
