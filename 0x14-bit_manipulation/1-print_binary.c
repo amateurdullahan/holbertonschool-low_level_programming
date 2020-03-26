fffc034390febff49752d6bf325cc8d85a9b9b5a
amateurdullahan <zabrak12@gmail.com>
2020-03-26 18:18:45 +0000

    add 1-print_binary, 1-main testfile, updated holberton header with prototype

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_binary - print a binary representation of a number
+ * @n: number to be printed as binary
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+int a = 0, count, b, temp;
+
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+
+temp = n;
+
+while (temp != 0)
+{
+a++;
+temp = temp >> 1;
+}
+
+for (count = a - 1; count >= 0; count--)
+{
+b = n >> count;
+if (b & 1)
+_putchar('1');
+else
+_putchar('0');
+}
+}
