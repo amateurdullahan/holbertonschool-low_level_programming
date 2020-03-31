fbe290ccd2faca61754c68cbab8f62d3f235c9a5
amateurdullahan <zabrak12@gmail.com>
2020-03-31 23:22:03 +0000

    add 0-read_textfile and required files for directory

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads text file and prints to POSIX stdout
+ * @filename: name of file
+ * @letters: num of chars in file
+ *
+ * Return: number of letters could read+print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+return (0);
+}
