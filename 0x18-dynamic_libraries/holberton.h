09c690f8dd7cedcb5e3a4ede13b79799cc400d60
amateurdullahan <zabrak12@gmail.com>
2020-05-04 17:10:09 +0000

    add base files

---
+++ 0x18-dynamic_libraries/holberton.h
@@ -0,0 +1,27 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* HOLBERTON_H */
