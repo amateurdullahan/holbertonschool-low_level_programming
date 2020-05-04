a6612f9de4d2f7b632e6a2d4e31c172f3ab3f50c
amateurdullahan <zabrak12@gmail.com>
2020-05-04 01:24:58 +0000

    add 4-free_dlistint, lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a dlistint_t list
+ * @head: head of dlistint_t list
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *next;
+while (head->next != NULL)
+{
+next = head->next;
+free(head);
+head = next;
+}
+free(head);
+}
