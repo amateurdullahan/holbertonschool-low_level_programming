880ae20df37f5a759bd043f871a71f01310f9f64
amateurdullahan <zabrak12@gmail.com>
2020-05-29 17:41:07 +0000

    reworked and added code

--- 0x1A-hash_tables/0-hash_tables_create.c
+++ 0x1A-hash_tables/0-hash_tables_create.c
@@ -9,9 +9,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new = malloc(sizeof(char) * size);
+hash_table_t *new = NULL;
+unsigned int a = 0;
+
+new = malloc(sizeof(hash_table_t));
 if (new == NULL)
 return (NULL);
 new->size = size;
+new->array = malloc(sizeof(hash_node_t *) * size);
+if (new->array == NULL)
+{
+free(new);
+return (NULL);
+}
+while (a < size)
+{
+new->array[a] = NULL;
+a++;
+}
 return (new);
 }
